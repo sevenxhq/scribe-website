61c2b9e93ad5fb4875c96f344b1e04c47af8b506
Arpit Jacob <arpit.jacob@gmail.com>
2023-10-30 07:40:08 +0200

    feature():version update (#9) (#7)
    Co-authored-by: Cedrick Tresor IGIRINEZA <111174174+Ntarecedrick@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,23 @@
+"use client";
+
 import FeatureSection from "@components/home/FeatureList";
 import SlidesGroup from "@components/home/Slides";
 import Footer from "@components/layout/Footer";
 import Menu from "@components/layout/Menu";
 import WebTab from "@components/Tab";
-import { GetDownloadUrlDocument } from "@graphql/graphql";
 import { allSlideLists, allHeros } from "contentlayer/generated";
 import { allFeatureLists } from "contentlayer/generated";
-import { client } from "src/client/graphql";
 
 
-async function getDownloadUrlQuery() {
-  const data = await client.request(GetDownloadUrlDocument);
-  return data;
-}
 
-export default async function Page() {
-  const downloadUrlQuery = await getDownloadUrlQuery();
+
+export default function Page() {
 
   return (
     <>
       <Menu />
 
-      <WebTab data={downloadUrlQuery} />
+      <WebTab/>
 
       <SlidesGroup hero={allHeros[0]} slides={allSlideLists} />
 
